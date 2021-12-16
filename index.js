4f98cee3bbda7dbe35270161379faf98f0bfed00
hakimuddinhh <hakimuddinhh@gmail.com>
2021-12-16 21:58:51 +0530

    added animation, sound and improved probablity

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const reloadBtn = document.querySelector('#reload-btn');
-const chances = [1,1,1,2,2,2,2,2,3,3,3,3,4,4,8];
+const chances = [1,1,2,1,2,3,3,2,3,4,8,4];
 
 // 1 => 2
 // 2 => 3
@@ -14,12 +14,17 @@ reloadBtn.addEventListener('click', (e) => {
 })
 
 function getChanceValue() {
-	const randomIdx = Math.floor((Math.random() * 15));
+	const randomIdx = Math.floor((Math.random() * chances.length));
 	return chances[randomIdx];
 }
 
 function showUpdatedValue(updatedValue) {
 	document.querySelectorAll('.score').forEach(function(elem, idx) {
 			elem.innerText = updatedValue;
-  })
+
+            elem.classList.remove('animate-n'); // reset animation
+            void elem.offsetWidth; // trigger reflow
+            elem.classList.add('animate-n'); // start animation
+    })
+    new Audio("./roll.wav").play()
 }
